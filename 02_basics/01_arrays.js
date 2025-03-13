f6259f551c33240a0fee1fdcec9225888212c0d8
RYEmber07 <ryember07@gmail.com>
2025-03-13 11:49:43 +0000

    done few topics of arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,7 +1,8 @@
 // array
 
 const myArr = [0, 1, 2, 3, 4, 5]
-const myHeors = ["shaktiman", "naagraj"]
+const myHeroes = ["shaktiman", "naagraj"]
+// creates shallow copy not a deep copy (heap memory,share same refrence)
 
 const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
@@ -13,27 +14,35 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.pop()
 
 // myArr.unshift(9)
+// unshift => pushes value to front (time-consuming operation as need to shift all values) 
 // myArr.shift()
+// like pop but for front 
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9)); // is 9 present ? returns boolean
+// console.log(myArr.indexOf(3)); // returns index if present if not then -1
 
 // const newArr = myArr.join()
 
 // console.log(myArr);
-// console.log( newArr);
-
+// console.log(newArr);
+// adds all elements of the array into a string [0,1,2,3] => 0,1,2,3
 
 // slice, splice
 
-console.log("A ", myArr);
+console.log("original array :", myArr);
 
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
-
+console.log("using slice :",myn1);
+console.log("original array after using slice ", myArr);
 
 const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+console.log("using splice :",myn2);
+console.log("original array after using splice :", myArr);
+
+
+//difference between slice and splice is splice modifes(the left part remains only) the original array and also includes the outer range
+
+// eg. if i use .splice(1,3) on 0,1,2,3,4
+//spliced portion is 1,2,3 the array gets modified to 0,4
+
--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,17 +1,20 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
-
+// marvel_heros.push(dc_heros) 
+// !!! does not merge the arrays !!!
+// [0,1,2,[3.0,3.1,3.2]] does this instead
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
 
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
+//.concat() returns a new array and does not modify , works on two arrays
+//merges the two arrays
 
 const all_new_heros = [...marvel_heros, ...dc_heros]
-
 // console.log(all_new_heros);
+// spread operator (...) merges multiple arrays
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
