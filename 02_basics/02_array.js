87caf0331f0d1dbaadbcdf59e3edf73cebd16e03
RYEmber07 <ryember07@gmail.com>
2025-03-18 20:27:01 +0000

    did till 1st vid of objects

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -10,25 +10,27 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 //.concat() returns a new array and does not modify , works on two arrays
-//merges the two arrays
+//merges the two arrays 
 
 const all_new_heros = [...marvel_heros, ...dc_heros]
 // console.log(all_new_heros);
 // spread operator (...) merges multiple arrays
+// order of using the spread operator (...) matters
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
+// .flat(x) flats the array upto x depth
 console.log(real_another_array);
 
 
-
-console.log(Array.isArray("Hitesh"))
+console.log(Array.isArray(["Hitesh"])) // true, checks if the passed thing is an array
 console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+// converts to array ['H','i','t'..... \,'h']
+console.log(Array.from({name: "hitesh"})) 
+// interesting result gives [] empty array , we also need to give some value with this so it understands array needs to be create by what keys or values.
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
-
 console.log(Array.of(score1, score2, score3));
--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,13 +1,12 @@
-// singleton
-// Object.create
+// singleton 
+// when we create a object using constructor (singletion object is not created by literals)
 
-// object literals
+// Object.create (constructor method)
 
+// object literals const JsUser = {}
 const mySym = Symbol("key1")
-
-
 const JsUser = {
-    name: "Hitesh",
+    name: "Hitesh", // no need to write name as "name" but it is being treated like that only 
     "full name": "Hitesh Choudhary",
     [mySym]: "mykey1",
     age: 18,
@@ -17,22 +16,34 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
+// Ways to access objects
+
 // console.log(JsUser.email)
-// console.log(JsUser["email"])
+// this method will not work for full name 
+
+// console.log(JsUser["email"]) // you have to mention "email" just email wont work
 // console.log(JsUser["full name"])
+
+// to use a symbol as a key if you just od 
+// mySym : "mykey1", and print the typeof JsUser.mySym it is string
+// To do this do [mySym] : "mykey1",
+// and it can only be accessed by this method
 // console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+// Object.freeze(JsUser) so no one can do changes on the Object
+JsUser.email = "hitesh@microsoft.com" // no error but chnage will not propogate
 // console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
+// will create a new key and its value is this function
 JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
+    // 'this' will be used to select the same object
 }
 
-console.log(JsUser.greeting());
+console.log(JsUser.greeting()); 
+// while refereing a method if you do just JsUser.greeting it will not execute the function it gives reference of function
 console.log(JsUser.greetingTwo());
